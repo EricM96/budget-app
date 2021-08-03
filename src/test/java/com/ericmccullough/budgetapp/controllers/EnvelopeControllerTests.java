c10da35faea324e99f56e23ba971ff5c97af874b
Eric McCullough <mccullougheric96@gmail.com>
2021-08-02 21:31:32 -0500

    Add tests for envelope controller.

--- src/test/java/com/ericmccullough/budgetapp/controllers/EnvelopeControllerTests.java
+++ src/test/java/com/ericmccullough/budgetapp/controllers/EnvelopeControllerTests.java
@@ -15,7 +15,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -34,11 +36,72 @@ public class EnvelopeControllerTests {
         doNothing().when(mockEnvelopeService).newEnvelope(isA(Envelope.class));
 
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/envelope")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(payload)
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
         ).andExpect(status().isOk());
-        Mockito.verify(mockEnvelopeService, times(1))
-            .newEnvelope(any(Envelope.class));
+        Mockito.verify(mockEnvelopeService, times(1)).newEnvelope(any(Envelope.class));
+    }
+
+    @Test
+    public void createEnvelope_whenPayloadIsMissing_shouldReturnBadRequest() throws Exception {
+        final String payload = "";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        ).andExpect(status().isBadRequest());
+        Mockito.verify(mockEnvelopeService, times(0)).newEnvelope(any(Envelope.class));
+    }
+
+    @Test
+    public void createEnvelope_whenPayloadIsMissingName_shouldReturnBadRequest() throws Exception {
+        final String payload = "{ \"balance\": \"2000.00\" }";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        ).andExpect(status().isBadRequest());
+        Mockito.verify(mockEnvelopeService, never()).newEnvelope(any(Envelope.class));
+    }
+
+    @Test
+    public void createEnvelope_whenPayloadHasEmptyName_shouldReturnBadRequest() throws Exception {
+        final String payload = "{ \"name\": \"\", \"balance\": \"2000.00\" }";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        ).andExpect(status().isBadRequest());
+        Mockito.verify(mockEnvelopeService, never()).newEnvelope(any(Envelope.class));
+    }
+
+    @Test
+    public void createEnvelope_whenPayloadHasBlankName_shouldReturnBadRequest() throws Exception {
+        final String payload = "{ \"name\": \"       \", \"balance\": \"2000.00\" }";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        ).andExpect(status().isBadRequest());
+        Mockito.verify(mockEnvelopeService, never()).newEnvelope(any(Envelope.class));
+    }
+
+    @Test
+    public void createEnvelope_whenPayloadIsMissingBalance_shouldHaveBalanceOfZero() throws Exception {
+        final String payload = "{ \"name\": \"newEnvelope\" }";
+        doNothing().when(mockEnvelopeService).newEnvelope(isA(Envelope.class));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/envelope")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload)
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value("0"));
+        Mockito.verify(mockEnvelopeService, times(1)).newEnvelope(any(Envelope.class));
     }
 }
